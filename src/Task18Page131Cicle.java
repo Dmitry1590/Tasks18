f394eab21f5a5dffc764fb8818f3e278b1e1af1d
Dmitry1590 <dmitry.kasckiv@yandex.ru>
2023-05-04 00:37:54 +0500

    Ben, I do  it!

--- src/Task18Page131Cicle.java
+++ src/Task18Page131Cicle.java
@@ -10,24 +10,22 @@ public class Task18Page131Cicle {
         int[] massive = new int[number];
         int[] massive2 = new int[number];
 
-        for (int i = 0; i < massive.length - 1; i++) {
+        for (int i = 0; i < massive.length; i++) {
             massive[i] = random2.nextInt(100);
             System.out.println("Текущее значение: " + massive[i]);
-            for (int j = 2; j <= massive[i] - 1; j++) {
-                System.out.println("Делим на j " + j);//вывожу элемент
-
+            for (int j = 2; j < massive[i]; j++) {
+                //System.out.println("Делим на j " + j);//вывожу элемент
                 if (massive[i] % j != 0) {
                     if (j == massive[i] - 1) {
                         System.out.println("Число является простым");
                         massive2[count] = massive[i];
                         count++;
-                    } else System.out.println("Переходим к следующему делителю j");
+                    } //else System.out.println("Переходим к следующему делителю j");
                     continue;
                 }
                 System.out.println("Число не является простым, переходим к следующему");
                 break;
-//                    massive2[count] = massive[i];
-//                    count++;
+
             }
         }
         System.out.println(Arrays.toString(massive));//красиво выводит массив в формате [n1, n2, n3.....n]
