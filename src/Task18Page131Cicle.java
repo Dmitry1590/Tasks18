2691621e00c485b4442391512286995472d8aabd
Dmitry1590 <dmitry.kasckiv@yandex.ru>
2023-05-04 00:24:28 +0500

    Ben, I don't need Help!

--- src/Task18Page131Cicle.java
+++ src/Task18Page131Cicle.java
@@ -1,26 +1,37 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task18Page131Cicle {
     public static void main(String[] args) {
-        int n=100;
-        int number=20;
-        int count=0;
-        Random random2=new Random();
-        int [] massive=new int[number];
-        int [] massive2=new int[20];
+        //int n = 100;
+        int number = 20;
+        int count = 0;
+        Random random2 = new Random();
+        int[] massive = new int[number];
+        int[] massive2 = new int[number];
 
-for (int i=0; i<massive.length-1;i++) {
-    massive[i] = random2.nextInt(100);
+        for (int i = 0; i < massive.length - 1; i++) {
+            massive[i] = random2.nextInt(100);
+            System.out.println("Текущее значение: " + massive[i]);
+            for (int j = 2; j <= massive[i] - 1; j++) {
+                System.out.println("Делим на j " + j);//вывожу элемент
 
-    for (int j = 2; j < massive[i]-1; j++) {
-        if (massive[i] % j == 0) {
-            massive2[count] = massive[i];
-            count++;
+                if (massive[i] % j != 0) {
+                    if (j == massive[i] - 1) {
+                        System.out.println("Число является простым");
+                        massive2[count] = massive[i];
+                        count++;
+                    } else System.out.println("Переходим к следующему делителю j");
+                    continue;
+                }
+                System.out.println("Число не является простым, переходим к следующему");
+                break;
+//                    massive2[count] = massive[i];
+//                    count++;
+            }
         }
-
-    }
-}
         System.out.println(Arrays.toString(massive));//красиво выводит массив в формате [n1, n2, n3.....n]
         System.out.println("Из них простые числа:");
         System.out.println(Arrays.toString(massive2));
-}
+    }
 }
